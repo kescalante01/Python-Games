0c5043b1364a083b22383667cf2f86b51e52c8e3
Your Actual Name <Your Actual Email>
2017-12-24 13:19:47 -0500

    update: removed spoke, lizzard options

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,14 +2,13 @@ import random
 RPS_Wins = {
     "rock": "scissors",
     "scissors": "paper",
-    "paper" : "rock"
+    "paper" : "rock",
+
 }
 RPS_choices = ("paper","scissors","rock")
 computer_choice = RPS_choices[random.randint(0,2)]
-print(computer_choice)
 computer_choice = computer_choice.lower()
-user_choice = input("Pick Paper, Rock, or Scissors")
-print(user_choice)
+user_choice = input("Pick Paper, Rock, Scissors: ")
 user_choice = user_choice.lower()
 if RPS_Wins[computer_choice] == user_choice:
     print("Computer Wins")
