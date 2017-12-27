d97ecbcb915b82ca798c37cb2a3dd0a655581017
miami401k <34661756+miami401k@users.noreply.github.com>
2017-12-26 17:40:20 -0700

    Update cointoss.py

--- cointoss.py
+++ cointoss.py
@@ -3,11 +3,11 @@ import random
 coin = ['tails', 'heads']
 
 while True:
-    x = input("Would you like to flip the coin?  Type Yes or No: ")
+    x = input("Would you like for the computer to flip the coin for you?  Type Yes in any version to continue. ")
     x = x.lower()
-    if x =='yes':
+    if x == 'yes' or x == 'Yes' or x == "YES" or x == "y" or x == "ye":
         cointoss = coin[random.randint(0, 1)]
-        print(cointoss)
+        print("And the results are.....  ", cointoss)
     else:
         print('Thanks for playing!')
         exit()
